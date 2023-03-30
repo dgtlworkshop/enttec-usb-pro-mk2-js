dffbbee96ff001d8fc1dcd852844a9169d5ac2ca
Matthew Conto <conto@hey.com>
2023-03-30 13:31:21 -0700

    use commander

--- tests/cycle-colors.ts
+++ tests/cycle-colors.ts
@@ -1,17 +1,35 @@
 import { connect } from "../src/helpers/connect.js";
 import { timeout } from "../src/helpers/timeout.js";
+import { program } from "commander";
+import type { EnttecUsbMk2Pro } from "../src/EnttecProUSB.js";
 
-const enttec = await connect();
+program
+	.name("enttec-usb-pro-mk2 cycle-addresses")
+	.description("Cycles DMX values")
+	.option("-d --delay", "Delay in ms between changes", "1000")
+	.action(async (delay, options) => {
+		let enttec: EnttecUsbMk2Pro;
+		try {
+			enttec = await connect();
+		} catch (error) {
+			program.error(String(error));
+		}
+		const buffer = enttec.getAll(0);
+		delay = parseInt(delay);
 
-const buffer = enttec.getAll(0);
+		console.info("Starting with delay of", delay, "ms");
 
-while (true) {
-	for (let address = 0; address < buffer.length; address++) {
-		console.info("Setting", address, "to", 255);
-		buffer[address] = 255;
-		await enttec.setAll(0, buffer);
-		await timeout(1000);
-		console.info("Setting", address, "to", 0);
-		await enttec.setAll(0, buffer);
-	}
-}
+		while (true) {
+			for (let address = 1; address < buffer.length; address++) {
+				console.info("Setting", address, "to", 255);
+				buffer[address] = 255;
+				await enttec.setAll(0, buffer);
+				await timeout(delay);
+				console.info("Setting", address, "to", 0);
+				buffer[address] = 0;
+				await enttec.setAll(0, buffer);
+			}
+		}
+	});
+
+program.parse();
